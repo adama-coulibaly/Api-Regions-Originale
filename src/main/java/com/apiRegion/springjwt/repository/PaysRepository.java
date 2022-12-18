4c354d0296c03487400c5d1c57ee897a1932257e
adcoulibaly <coulibalyadamabekaye03@gmail.com>
2022-12-18 16:56:16 +0000

    Commentaire gerer

---
+++ src/main/java/com/apiRegion/springjwt/repository/PaysRepository.java
@@ -0,0 +1,17 @@
+package com.apiRegion.springjwt.repository;
+
+import com.apiRegion.springjwt.models.Pays;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaysRepository extends JpaRepository<Pays, Long> {
+   /* @Query(value =" SELECT * FROM `pays` WHERE pays.nom_pays=:nomPays",nativeQuery = true)
+    List<String> VerifierPay(String nomPays);
+
+    */
+
+    public Pays findByNompays(String pays);
+
+
+}
